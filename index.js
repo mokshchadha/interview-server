b1d69f2ec4bfd0a3c409035ab9ee672ca9ca28fb
moksh <moksh@pop-os.localdomain>
2021-11-14 15:33:15 +0530

    backend db connection and route setup

---
+++ index.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+const port = 7000;
+
+const { sendTheData } = require("./repo");
+
+const connectionString =
+  "mongodb+srv://moksh:test1234temp4321@cluster0.httpz.mongodb.net/" +
+  "sample_training?retryWrites=true&w=majority";
+
+mongoose.connect(process.env.MONGODB_URI || connectionString, {
+  useNewUrlParser: true,
+  //   useCreateIndex: true,
+  //   useUnifiedTopology: true,
+  //   useFindAndModify: false,
+});
+
+async function printRequest(req) {
+  const ip =
+    (req.headers["x-forwarded-for"] || "").split(",").pop() ||
+    req.connection?.remoteAddress ||
+    req.socket?.remoteAddress ||
+    req.connection?.socket?.remoteAddress;
+  const date = new Date();
+  console.log(`[${date.toDateString()}] ${ip} ${req.method} ${req.url}`);
+}
+
+function logger(req, res, next) {
+  printRequest(req);
+  next();
+}
+
+app.use(logger);
+
+app.get("/", (req, res) => {
+  res.setHeader("Content-Type", "application/json; charset=utf-8");
+  res.end(JSON.stringify({ msg: "success" }));
+});
+
+app.get("/listCompanies", async function (req, res) {
+  const data = await mongoose.connection.db.collection("companies").find({}).limit(5).toArray()
+  console.log({ data });
+  res.setHeader("Content-Type", "application/json; charset=utf-8");
+  res.end(JSON.stringify({ data }));
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
---
+++ repo/index.js
@@ -0,0 +1,7 @@
+//mongodb+srv://moksh:<password>@cluster0.httpz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+
+function sendTheData(){
+    return  ""
+}
+
+module.exports = {sendTheData}
